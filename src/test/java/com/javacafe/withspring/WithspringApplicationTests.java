86f8bcc83c8cc2212d6bb95b69e5478fed09c7ec
백재현 <xkgun@naver.com>
2017-06-09 23:06:52 +0900

    Initial commit

---
+++ src/test/java/com/javacafe/withspring/WithspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javacafe.withspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WithspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
